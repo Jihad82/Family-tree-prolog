1d518fdebc8aec1573a8dd824d2102bdb20d144f
Aabharan Zihad <jihad.202002082@green.ac.bd>
2023-06-09 23:56:58 +0600

    Create family-tree.pl

---
+++ family-tree.pl
@@ -0,0 +1,23 @@
+/* Facts */
+
+male(jack).
+male(oliver).
+male(ali).
+male(james).
+male(simon).
+male(harry).
+female(helen).
+female(sophie).
+female(jess).
+female(lily).
+ 
+parent_of(jack,jess).
+parent_of(jack,lily).
+parent_of(helen, jess).
+parent_of(helen, lily).
+parent_of(oliver,james).
+parent_of(sophie, james).
+parent_of(jess, simon).
+parent_of(ali, simon).
+parent_of(lily, harry).
+parent_of(james, harry).
